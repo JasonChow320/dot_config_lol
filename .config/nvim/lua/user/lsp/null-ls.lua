1ff6cc2eb99af5f10fbe05b0d0c2fbacb43b9055
Jason Chow <jasonchow9168@gmail.com>
2023-07-09 16:59:21 -0400

    feat: add gitsigns to null-ls

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -14,6 +14,7 @@ null_ls.setup({
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
 		formatting.stylua.with({ extra_args = { "--indent-type Spaces" } }),
-    -- diagnostics.flake8
+        null_ls.builtins.code_actions.gitsigns,
+        -- diagnostics.flake8
 	},
 })
