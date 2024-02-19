0e001c6fec434f01b45da189949693e0ed9bb566
Jason Chow <jasonchow9168@gmail.com>
2024-02-19 16:56:28 -0500

    feat: improve install script

--- dot_files/install.sh
+++ dot_files/install.sh
@@ -1,13 +1,25 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # assume everything in this directory can be copied to ${HOME}
 
+function create_symlink()
+{
+    if [ -d $1 ];
+    then
+        ln -rs $1/* $2
+    else
+        ln -s $1 $2
+    fi
+}
+
 DOT_PATTERN="dot_"
+CUR_DIR="${CUSTOMRC_DIR}/dot_files/"
 for file in *;
 do
     if [[ ${file} == *"${DOT_PATTERN}"* ]];
     then
-        cp -r ${file} ${HOME}/.${file/$DOT_PATTERN/}
+        #cp -r ${file} ${HOME}/.${file/$DOT_PATTERN/}
+        create_symlink ${CUR_DIR}/${file} ${HOME}/.${file/$DOT_PATTERN/}
     fi
 done
 
