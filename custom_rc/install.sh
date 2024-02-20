ce03f8f2d56606918a831e424e7e3079719fc756
Jason Chow <79327473+JasonChow320@users.noreply.github.com>
2024-02-20 08:31:05 -0500

    Update install.sh

--- custom_rc/install.sh
+++ custom_rc/install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -i
 
-cat custom_bashrc >> ${HOME}/.bashrc
-echo "source $(pwd)/custom_rc" >> ${HOME}/.bashrc
+source ~/.bashrc
+
+cat ${CUSTOMRC_DIR}/custom_rc/custom_bashrc >> ${HOME}/.bashrc
+echo "source ${CUSTOMRC_DIR}/custom_rc/custom_rc" >> ${HOME}/.bashrc
 
 source ~/.bashrc
