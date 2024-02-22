85d127e36463984f313ba6705c21d3dffc6a5b9a
Jason Chow <79327473+JasonChow320@users.noreply.github.com>
2024-02-22 09:30:57 -0500

    Update install.sh

--- dot_files/install.sh
+++ dot_files/install.sh
@@ -1,25 +1,39 @@
-#!/bin/bash -x
+#!/bin/bash
+
+# regular colors
+Red='\033[0;31m'    # Red
+Green='\033[0;32m'  # Green
+Yellow='\033[0;33m' # Yellow
+NC='\033[0m'        # No Color
 
 # assume everything in this directory can be copied to ${HOME}
-
-function create_symlink()
-{
-    if [ -d $1 ];
-    then
-        cp -r $1/* $2
-    else
-        cp $1 $2
-    fi
-}
-
 DOT_PATTERN="dot_"
 CUR_DIR="${CUSTOMRC_DIR}/dot_files/"
+DOT_FILES=(
+    "dot_config/nvim"
+    "dot_tmux.conf"
+    "dot_vimrc"
+    "dot_clang-format"
+    "dot_gitignore"
+)
+
+for dot_file in "${DOT_FILES[@]}"
+do
+    if [ ! -e ${CUR_DIR}$dot_file ];
+    then
+        echo -e "${Red}Dot file ${dot_file} not found in ${CUR_DIR}.${NC}"
+    else
+        user_file="${HOME}/.${dot_file/$DOT_PATTERN/}"
+        rm -rf user_file
+        ln -sf ${CUR_DIR}/$dot_file ${user_file}
+    fi
+done
+
 for file in *;
 do
-    if [[ ${file} == *"${DOT_PATTERN}"* ]];
+    if [[ ${file} == *"${DOT_PATTERN}"* ]] && [[ ! ${DOT_FILES[@]} =~ $file ]];
     then
-        #cp -r ${file} ${HOME}/.${file/$DOT_PATTERN/}
-        create_symlink ${CUR_DIR}/${file} ${HOME}/.${file/$DOT_PATTERN/}
+        echo -e "${Red}${file} is not defined in DOT_FILES.${NC}"
     fi
 done
 
