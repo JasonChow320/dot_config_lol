ab616229b3428dc3d1061988af62d651416c6fdc
Jason Chow <79327473+JasonChow320@users.noreply.github.com>
2024-03-01 11:08:57 -0500

    Update install.sh

--- custom_rc/install.sh
+++ custom_rc/install.sh
@@ -5,4 +5,8 @@ source ~/.bashrc
 cat ${CUSTOMRC_DIR}/custom_rc/custom_bashrc >> ${HOME}/.bashrc
 echo "source ${CUSTOMRC_DIR}/custom_rc/custom_rc" >> ${HOME}/.bashrc
 
+cat ${CUSTOMRC_DIR}/custom_rc/custom_bashrc >> ${ZDOTDIR}/.zshrc
+echo "source ${CUSTOMRC_DIR}/custom_rc/custom_rc" >> ${ZDOTDIR}/.zshrc
+
 source ~/.bashrc
+source ${ZDOTDIR}/.zshrc
