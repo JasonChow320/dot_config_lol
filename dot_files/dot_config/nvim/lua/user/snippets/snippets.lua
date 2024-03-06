faf4bf36d619d72c2a5ec93b208a2069240bb31e
Jason Chow <79327473+JasonChow320@users.noreply.github.com>
2024-03-06 17:57:38 -0500

    Create snippets.lua
    add snippets.lua for luasnip

---
+++ dot_files/dot_config/nvim/lua/user/snippets/snippets.lua
@@ -0,0 +1,200 @@
+local ls = require("luasnip")
+-- some shorthands...
+local s = ls.snippet
+local sn = ls.snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local l = require("luasnip.extras").lambda
+local rep = require("luasnip.extras").rep
+local p = require("luasnip.extras").partial
+local m = require("luasnip.extras").match
+local n = require("luasnip.extras").nonempty
+local dl = require("luasnip.extras").dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local types = require("luasnip.util.types")
+local conds = require("luasnip.extras.conditions")
+local conds_expand = require("luasnip.extras.conditions.expand")
+
+-- If you're reading this file for the first time, best skip to around line 190
+-- where the actual snippet-definitions start.
+
+-- Every unspecified option will be set to the default.
+ls.setup({
+	keep_roots = true,
+	link_roots = true,
+	link_children = true,
+
+	-- Update more often, :h events for more info.
+	update_events = "TextChanged,TextChangedI",
+	-- Snippets aren't automatically removed if their text is deleted.
+	-- `delete_check_events` determines on which events (:h events) a check for
+	-- deleted snippets is performed.
+	-- This can be especially useful when `history` is enabled.
+	delete_check_events = "TextChanged",
+	ext_opts = {
+		[types.choiceNode] = {
+			active = {
+				virt_text = { { "choiceNode", "Comment" } },
+			},
+		},
+	},
+	-- treesitter-hl has 100, use something higher (default is 200).
+	ext_base_prio = 300,
+	-- minimal increase in priority.
+	ext_prio_increase = 1,
+	enable_autosnippets = true,
+	-- mapping for cutting selected text so it's usable as SELECT_DEDENT,
+	-- SELECT_RAW or TM_SELECTED_TEXT (mapped via xmap).
+	store_selection_keys = "<Tab>",
+	-- luasnip uses this function to get the currently active filetype. This
+	-- is the (rather uninteresting) default, but it's possible to use
+	-- eg. treesitter for getting the current filetype by setting ft_func to
+	-- require("luasnip.extras.filetype_functions").from_cursor (requires
+	-- `nvim-treesitter/nvim-treesitter`). This allows correctly resolving
+	-- the current filetype in eg. a markdown-code block or `vim.cmd()`.
+	ft_func = function()
+		return vim.split(vim.bo.filetype, ".", true)
+	end,
+})
+
+-- complicated function for dynamicNode.
+local function cdocsnip(args, _, old_state)
+	-- !!! old_state is used to preserve user-input here. DON'T DO IT THAT WAY!
+	-- Using a restoreNode instead is much easier.
+	-- View this only as an example on how old_state functions.
+	local nodes = {
+		t({ "/**", " * " }),
+		i(1, "@brief"),
+		t({ "", "" }),
+	}
+
+	-- These will be merged with the snippet; that way, should the snippet be updated,
+	-- some user input eg. text can be referred to in the new snippet.
+	local param_nodes = {}
+
+	if old_state then
+		nodes[2] = i(1, old_state.descr:get_text())
+	end
+	param_nodes.descr = nodes[2]
+
+	-- At least one param.
+	if string.find(args[2][1], " ") then
+		vim.list_extend(nodes, { t({ " * ", "" }) })
+	end
+
+	local insert = 2
+	for indx, arg in ipairs(vim.split(args[2][1], ", ", true)) do
+		-- Get actual name parameter.
+		arg = vim.split(arg, " ", true)[2]
+		if arg then
+			local inode
+			-- if there was some text in this parameter, use it as static_text for this new snippet.
+			if old_state and old_state[arg] then
+				inode = i(insert, old_state["arg" .. arg]:get_text())
+			else
+				inode = i(insert)
+			end
+			vim.list_extend(
+				nodes,
+				{ t({ " * @param " .. arg .. " " }), inode, t({ "", "" }) }
+			)
+			param_nodes["arg" .. arg] = inode
+
+			insert = insert + 1
+		end
+	end
+
+	if args[1][1] ~= "void" then
+		local inode
+		if old_state and old_state.ret then
+			inode = i(insert, old_state.ret:get_text())
+		else
+			inode = i(insert)
+		end
+
+		vim.list_extend(
+			nodes,
+			{ t({ " * ", " * @return " }), inode, t({ "", "" }) }
+		)
+		param_nodes.ret = inode
+		insert = insert + 1
+	end
+
+	vim.list_extend(nodes, { t({ " */" }) })
+
+	local snip = sn(nil, nodes)
+	-- Error on attempting overwrite.
+	snip.old_state = param_nodes
+	return snip
+end
+
+ls.add_snippets("c", {
+	-- Very long example for a java class.
+	s("fn", {
+		d(5, cdocsnip, { 2, 4 }),
+        t({ "", "" }),
+		c(1, {
+			i(nil, ""),
+			t("static "),
+		}),
+		c(2, {
+			t("void"),
+			t("char"),
+			t("char*"),
+			t("int"),
+			t("int*"),
+			t("double"),
+			t("boolean"),
+			t("bool"),
+			i(nil, ""),
+		}),
+		t(" "),
+		i(3, "myFunc"),
+		t("("),
+		i(4),
+		t(")"),
+		t({ "\t", "{", "\t" }),
+		i(0),
+		t({ "", "}" }),
+	}),
+}, {
+	key = "c",
+})
+
+-- set type to "autosnippets" for adding autotriggered snippets.
+ls.add_snippets("all", {
+	s("autotrigger", {
+		t("autosnippet"),
+	}),
+}, {
+	type = "autosnippets",
+	key = "all_auto",
+})
+
+-- in a lua file: search lua-, then c-, then all-snippets.
+ls.filetype_extend("lua", { "c" })
+ls.filetype_set("cpp", { "c" })
+
+require("luasnip.loaders.from_vscode").lazy_load()
+require("luasnip.loaders.from_snipmate").lazy_load()
+require("luasnip.loaders.from_lua").lazy_load()
+
+-- One peculiarity of honza/vim-snippets is that the file containing global
+-- snippets is _.snippets, so we need to tell luasnip that the filetype "_"
+-- contains global snippets:
+ls.filetype_extend("all", { "_" })
+
+vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+
+vim.keymap.set({"i", "s"}, "<C-E>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	end
+end, {silent = true})
