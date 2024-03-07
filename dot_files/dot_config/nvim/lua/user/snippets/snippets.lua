f8d1530c8eeefaa864f82a91c53e8b20538815bb
Jason Chow <79327473+JasonChow320@users.noreply.github.com>
2024-03-07 16:50:02 -0500

    Update snippets.lua

--- dot_files/dot_config/nvim/lua/user/snippets/snippets.lua
+++ dot_files/dot_config/nvim/lua/user/snippets/snippets.lua
@@ -189,12 +189,16 @@ require("luasnip.loaders.from_lua").lazy_load()
 -- contains global snippets:
 ls.filetype_extend("all", { "_" })
 
-vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-L>", function() ls.jump( 1) end, {silent = true})
-vim.keymap.set({"i", "s"}, "<C-J>", function() ls.jump(-1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-l>", function() ls.jump( 1) end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-h>", function() ls.jump(-1) end, {silent = true})
 
-vim.keymap.set({"i", "s"}, "<C-E>", function()
+vim.keymap.set({"i", "s"}, "<C-j>", function()
 	if ls.choice_active() then
 		ls.change_choice(1)
+	end
+end, {silent = true})
+vim.keymap.set({"i", "s"}, "<C-k>", function()
+	if ls.choice_active() then
+		ls.change_choice(-1)
 	end
 end, {silent = true})
